91910f7067f80300c6c00e5766d11e8cfdf6194e
DEII <1840094972@qq.com>
2022-12-21 13:44:09 +0800

    网易乐评

--- plugins/cloudMusicCom.py
+++ plugins/cloudMusicCom.py
@@ -1,7 +1,9 @@
+# -*- coding: gbk -*-
+import json
+
 import requests
-tocken='Imiewqr8fMpApy3kqEJh'
+tocken='Imi8fMpAfsdfpy3kqEJh'
 def getCom():
-    import requests
 
     url = "https://v2.alapi.cn/api/comment"
 
@@ -9,7 +11,12 @@ def getCom():
     headers = {'Content-Type': "application/x-www-form-urlencoded"}
 
     response = requests.request("POST", url, data=payload, headers=headers)
-
-    print(response.text)
+    a=response.json()
+    #dia=json.loads(response)
+    #re=dia.get("comment_content")
+    print(response.json())
+    data=a.get("data")
+    da=data.get('comment_content')+'\n          ---¡¶'+data.get("title")+'¡·\n              '+ data.get('published_date')
+    print(da)
 if __name__ == '__main__':
     getCom()
